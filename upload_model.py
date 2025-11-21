dee365f89dfa9ac7a0d7dd347ec587c87ced1e59
ali-abbi <ali.aghababaei96@gmail.com>
2025-11-21 23:00:29 +0100

    Update app to load model from HF; clean DS_Store

--- upload_model.py
+++ upload_model.py
@@ -1,9 +1,18 @@
-from huggingface_hub import HfApi
+from huggingface_hub import HfApi, upload_folder
+import os
+
+MODEL_PATH = "models/sentiment_distilbert"
+HF_REPO = "aliabbi/sentiment-distilbert"
+
+HF_TOKEN = os.getenv("HF_TOKEN")
 
 api = HfApi()
 
-api.upload_folder(
-    folder_path="models/sentiment_bert",
-    repo_id="aliabbi/sentiment-bert",
+upload_folder(
+    folder_path=MODEL_PATH,
+    repo_id=HF_REPO,
     repo_type="model",
+    token=HF_TOKEN
 )
+
+print("Model uploaded successfully!")
